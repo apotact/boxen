08205fd81d19fae3bd7f08b83867f7fa3efe74ac
Michael Pfister <pfista@gmail.com>
2015-04-03 16:39:31 -0500

    Updating file path

--- modules/projects/manifests/clave.pp
+++ modules/projects/manifests/clave.pp
@@ -4,9 +4,10 @@ class projects::clave {
 
   $home = '/Users/${::boxen_user}/'
   $srcdir = '${home}/src'
+  $clave = '${srcdir}/clave'
 
-  repository { '${srcdir}/clave':
+  repository { '${clave}':
       source => 'git@github.com:apotact/clave',
-      path => '/Users/${::boxen_user}/src/clave',
+      path => $clave,
     }
 }
