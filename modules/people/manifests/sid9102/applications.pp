4cb1d1f85e77aa3757c71792d7bca665adbb7f22
Michael Pfister <pfista@gmail.com>
2015-04-06 16:47:19 -0500

    removing clear dock /mag prefs

--- modules/people/manifests/sid9102/applications.pp
+++ modules/people/manifests/sid9102/applications.pp
@@ -7,19 +7,13 @@ class people::sid9102::applications {
   include osx::global::disable_remote_control_ir_receiver
   include osx::global::disable_autocorrect
   include osx::global::tap_to_click
-  include osx::dock::clear_dock
   include osx::finder::no_file_extension_warnings
   include osx::safari::enable_developer_mode
-  class { 'osx::dock::position':
-    position => 'right',
-  }
+
   # Make the top right corner start the screen saver and the bottom left corner
   # launch Mission Control
   class { 'osx::dock::hot_corners':
     top_left => "Start Screen Saver",
-  }
-  class { 'osx::dock::magnification':
-    magnification => false
   }
 
   include onepassword
